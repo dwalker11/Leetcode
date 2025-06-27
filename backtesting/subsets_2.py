608e9b2047871a4c0362981bca1b30c606153e79
Devon <devon.dana@gmail.com>
2025-06-27 13:30:09 -0400

    Solve "Subsets II" problem - first attempt

---
+++ backtesting/subsets_2.py
@@ -0,0 +1,40 @@
+from typing import List
+
+
+def subsetsWithDup(nums: List[int]) -> List[List[int]]:
+    results = [[]]
+
+    nums.sort()
+
+    def dfs(i, subset):
+        if i == len(nums):
+            return
+
+        subset.append(nums[i])
+        results.append(subset.copy())
+
+        dfs(i+1, subset)
+
+        subset.pop()
+
+        # Advance the pointer until next value is not equal to the current
+        while i+1 < len(nums) and nums[i+1] == nums[i]:
+            i += 1
+
+        dfs(i+1, subset)
+
+    dfs(0, [])
+
+    return results
+
+
+def main():
+    results = subsetsWithDup([1, 2, 2])
+    print(results)
+
+    results = subsetsWithDup([1, 1])
+    print(results)
+
+
+if __name__ == "__main__":
+    main()

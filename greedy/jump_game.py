2cde2861ddfc80c882e38c090edb1425dfd5c20e
Devon <devon.dana@gmail.com>
2025-07-19 16:49:02 -0400

    Solve "Jump game" - final attempt

--- greedy/jump_game.py
+++ greedy/jump_game.py
@@ -2,27 +2,13 @@ from typing import List
 
 
 def canJump(nums: List[int]) -> bool:
-    if len(nums) == 1:
-        return True
+    goal = len(nums) - 1
 
-    start, end = 0, len(nums) - 1
+    for i in range(len(nums) - 2, -1, -1):
+        if i + nums[i] >= goal:
+            goal = i
 
-    curr = start
-    max_jump_distance = nums[curr]
-
-    while True:
-        if max_jump_distance == 0:
-            break
-
-        new_position = curr + max_jump_distance
-
-        if new_position >= end:
-            return True
-
-        curr = new_position
-        max_jump_distance = nums[curr]
-
-    return False
+    return goal == 0
 
 
 def main():
